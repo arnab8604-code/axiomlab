44180f139dc5aa585118408760c88c0c6b966f00
Arnab <arnab8604@gmail.com>
2026-07-21 13:14:08 +0530

    Version-7

---
+++ src/temp-data/clientele/clientele.ts
@@ -0,0 +1,45 @@
+import { Clientele } from "@/types/clientele";
+export const clienteleData : Clientele[] = [
+  {
+    title: "Major Private Residential Estates",
+    service:
+      "Asbestos Containing Material Visual Inspection & Survey Assessment.",
+    description:"Involved in the entire residential estate on asbestos inspection and survey to assess the overall exposure risk to health, safety and environmental conditions within the various residential premises and to protect public interest on health hazards as well as to provide mitigation measures and recommendations, where relevant.",
+    image:"",
+  },
+  {
+    title: "Alexandra Hospital and National University Hospital",
+    service:
+      "Airborne and Surface Microbiological Assessment",
+    description:"Involved in the study to assess airborne and surface microbiological assessment for baseline and contaminated cases within the hospitals.",
+       image:"",
+  },
+  {
+    title: "SMRT",
+    service:
+      "Airborne Dust Exposure Study",
+    description:"Involved in the study to assess personnel exposure to Total Airborne Dust for time weighted average, TWA during the maintenance operations in the tunnel.",
+    image:"",
+  },
+  {
+    title: "Ministry of Defence",
+    service:
+      "Indoor Air Quality Survey",
+    description:"Involved in an air quality survey to establish the relationship of equipment breakdown to poor Indoor Air Quality.",
+    image:"",
+  },
+  {
+    title: "INPEX Aceh Ltd",
+   service:
+      "Deep Water Oceanographic Survey",
+    description:"Involved as a Marine Scientist in the data processing and interpretation of rawdata collected from the RDI ADCP, Aanderaa RCM-7 Current Meter, Thermistor string and WLR-7 Tide Gauge for the measurement period of approximately one month.  The purpose of the study was to produce a database of current speed and direction, temperature as well as tidal elevations suitable for operational and design applications.",
+    image:"",
+  },
+  {
+    title: "Nanyang Technological University, School of Civil & Structural Engineering",
+    service:
+      "Current and Water Quality Measurement at Marina Bay",
+    description:"Involved in the field work to collect water samples for turbidity analysis as well as in-situ measurements of physical parameters over a period of 24 hours tidal cycle at 8 locations in the vicinity of the Marine bay including downstream of the Singapore River.",
+    image:"",
+  },
+];
---
+++ src/types/clientele.ts
@@ -0,0 +1,6 @@
+export interface Clientele {
+  title: string;
+  image:string;
+  description: string;
+  service:string;
+}
